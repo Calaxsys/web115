da499c28bc5ff659408e566d63b987597e6b58db
Chris <ffson1999@yahoo.com>
2025-02-15 07:46:12 -0500

    Finished fizzbuzz0 assignment part 1

--- scripts/fizzbuzz.js
+++ scripts/fizzbuzz.js
@@ -1,9 +1,9 @@
-document.getElementById('name_form').addEventListener('submit', function (event) {
+document.getElementById('name-form').addEventListener('submit', function (event) {
   event.preventDefault();
 
-  const firstName = document.getElementById('first_name').value;
-  const middleInitial = document.getElementById('middle_initial').value;
-  const lastName = document.getElementById('last_name').value;
+  const firstName = document.getElementById('first-name').value;
+  const middleInitial = document.getElementById('middle-initial').value;
+  const lastName = document.getElementById('last-name').value;
 
   let fullName = firstName;
   if (middleInitial) {
@@ -12,4 +12,15 @@ document.getElementById('name_form').addEventListener('submit', function (event)
   fullName += ` ${lastName}`;
 
   document.getElementById('greeting').textContent = `${fullName}!`;
+
+  const wordList = document.getElementById('word-list');
+  wordList.innerHTML = "";
+
+  const words = ["Game Design", "Level Creation", "Boss Battles", "Open Worlds", "Player Choice", "Story Mode", "Power Ups", "Next Level"];
+
+  for (let i = 1; i <= 125; i++) {
+    let listItem = document.createElement('li');
+    listItem.textContent = `${words[i % words.length]}`;
+    wordList.appendChild(listItem);
+  }
 });
